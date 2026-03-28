770221179eabbbed2e4a933aa900dc65413a9abd
Dhurkesh-R <rdhurkesh8@gmail.com>
2026-03-28 12:29:00 +0530

    Update AdminDashboard.jsx

--- frontend/src/adminPages/AdminDashboard.jsx
+++ frontend/src/adminPages/AdminDashboard.jsx
@@ -17,6 +17,7 @@ export default function AdminDashboard() {
       } catch (err) {
       console.error("Failed to fetch users", err);
       };
+    }
   }, []);
 
   const filteredUsers = users.filter(u => 
