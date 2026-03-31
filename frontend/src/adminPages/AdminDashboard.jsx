22573ed97814af981b2a2b8fc4b23bc3997ad88f
Dhurkesh-R <rdhurkesh8@gmail.com>
2026-03-31 15:25:24 +0530

    Update AdminDashboard.jsx

--- frontend/src/adminPages/AdminDashboard.jsx
+++ frontend/src/adminPages/AdminDashboard.jsx
@@ -76,6 +76,7 @@ export default function AdminDashboard() {
     <div className={`p-6 min-h-screen ${bg} ${text} md:ml-16`}>
       <header className="mb-8 flex flex-col lg:flex-row lg:items-center justify-between gap-6">
         <div>
+          <div className="w-12 h-10 md:hidden" />
           <h1 className="text-3xl font-bold flex items-center gap-3">
             <ShieldCheck className="text-red-500" size={32} /> Admin Control
           </h1>
