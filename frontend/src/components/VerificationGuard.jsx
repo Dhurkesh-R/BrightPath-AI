2520d7690ceb24a7270c8658e7a5cbfa7d9f8820
Dhurkesh-R <rdhurkesh8@gmail.com>
2026-04-04 23:13:58 +0530

    Update VerificationGuard.jsx

--- frontend/src/components/VerificationGuard.jsx
+++ frontend/src/components/VerificationGuard.jsx
@@ -6,6 +6,7 @@ import { useAuth } from '../contexts/AuthContext'; // Assuming you have an AuthC
 export default function VerificationGuard({ children }) {
   const { user, logout } = useAuth();
   const parsedUser = JSON.parse(user)
+  const isVerified = parsedUser.is_verified
 
   console.log(parsedUser)
   console.log(user)
