b838a29ece20b15714938f55fc9e8dbfd9936c67
Dhurkesh-R <rdhurkesh8@gmail.com>
2026-05-03 09:33:01 +0530

    Update index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,16 +1,20 @@
+// frontend/src/index.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'; 
 import { ThemeProviderWrapper } from "./contexts/ThemeContext";
-import { AuthProvider } from "./contexts/AuthContext"; // Import it here
+import { AuthProvider } from "./contexts/AuthContext";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProviderWrapper>
       <AuthProvider>
-        <App />
+        <Router>
+          <App />
+        </Router>
       </AuthProvider>
     </ThemeProviderWrapper>
   </React.StrictMode>
