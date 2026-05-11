9b09c4a143b464fbb8a1e54c7c8b0526407e89fc
Dhurkesh-R <rdhurkesh8@gmail.com>
2026-05-11 22:45:50 +0530

    Update TeacherDashboard.jsx

--- frontend/src/teacherPages/TeacherDashboard.jsx
+++ frontend/src/teacherPages/TeacherDashboard.jsx
@@ -151,7 +151,7 @@ export default function TeacherDashboard() {
             Performance Overview
           </h2>
           <div className="h-[300px] w-full">
-            <ResponsiveContainer width="100%" height="100%" className={`${textSecondary}`}>
+            <ResponsiveContainer width="100%" height="100%" className={`${text} ${cardBg}`}>
               <LineChart data={chartData} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
                 <CartesianGrid strokeDasharray="3 3" vertical={false} strokeOpacity={0.1} />
                 <XAxis 
